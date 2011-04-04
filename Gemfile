e2618200b33303f20f0ff6b2b738fc900241f949
Zac Clark <hi@zacclark.com>
2011-04-04 10:43:50 -0600

    Add Devise.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.5'
 gem 'sqlite3'
+gem 'devise', '1.2.1'
 
 group :development, :test do
   gem 'ruby-debug'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Launchpad::Application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
